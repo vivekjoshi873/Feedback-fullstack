9293db814bc8999d8af8379c5cf5de76d19846e5
Vivek Joshi <vj69376@gmail.com>
2025-09-18 00:39:57 +0530

    Initial commit

---
+++ src/middleware.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+export { default } from "next-auth/middleware";
+export function middleware(request: NextRequest) {
+  return NextResponse.redirect(new URL("/home", request.url));
+}
+
+export const config = {
+  matcher: ["/sign-in", "/sign-up", "/", "/dasboard/:path*", "/verify/:path*"],
+};
