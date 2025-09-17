df7fde099a74c89fe8ba9219295a404af170efd8
Vivek Joshi <vj69376@gmail.com>
2025-09-17 13:09:18 +0530

    Initial commit

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,23 @@
+import { resend } from "@/lib/resend";
+import VerificationEmail from "../../emails/VerficationEmail";
+import { ApiResponse } from "@/types/ApiResponse";
+
+export async function sendVerificationEmail(
+    email:string,
+    username:string,
+    verifyCode:string
+):Promise<ApiResponse> {
+    try {
+        await resend.emails.send({
+        from: 'onboarding@resending.dev',
+        to: 'email',
+        subject: 'Feedback Message | Verification Code',
+        react: VerificationEmail({username,otp:verifyCode}),
+});
+          return {success:true,message:"Successfully send the verification email"}
+    } catch (emailError) {
+        console.error("Error sending verification email",emailError);
+        return {success:false,message:"failed to send the verification email"}
+    }
+    
+}
