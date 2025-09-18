9363cac770fb500e1e9779f1fbd67f366d85ba66
Vivek Joshi <vj69376@gmail.com>
2025-09-18 23:33:16 +0530

    Initial Commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,10 @@ import type { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export async function middleware(request: NextRequest) {
-  const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
+  const token = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
   const url = request.nextUrl;
 
   const isAuthPage =
