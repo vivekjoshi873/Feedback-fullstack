64a3fdacc9f774ce51c37044468f868be47cd898
Vivek Joshi <vj69376@gmail.com>
2025-09-22 21:26:47 +0530

    Inital commit

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,9 +1,19 @@
 "use client"
 import React from 'react'
+import { Message } from '@/models/User'
+import { useState } from 'react'
+import { toast } from "sonner";
 
 function page() {
+  const [messages,setMessages] = useState<Message[]>([])
+  const [isLoading,setIsLoading] = useState(false)
+  const [isSwitchLoading,setIsSwitchLoading] = useState(false)
+  const handleDeleteMessage =(messageId:string)=>{
+    setMessages(messages.filter((message)=>message._id!==messageId))
+  }
   return (
     <div>
+
       
     </div>
   )
