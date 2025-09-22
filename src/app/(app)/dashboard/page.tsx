27ca29bb188e8ff3c9aafce0cdb66092bff5c6f7
Vivek Joshi <vj69376@gmail.com>
2025-09-22 23:46:54 +0530

    Inital Commit

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,22 +1,43 @@
-"use client"
-import React from 'react'
-import { Message } from '@/models/User'
-import { useState } from 'react'
+"use client";
+import React, { useCallback } from "react";
+import { Message } from "@/models/User";
+import { useState } from "react";
 import { toast } from "sonner";
+import { useSession } from "next-auth/react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { acceptMessageSchema } from "@/schemas/acceptMessageSchema";
+import axios, { AxiosError } from "axios";
+import { ApiResponse } from "@/types/ApiResponse";
 
 function page() {
-  const [messages,setMessages] = useState<Message[]>([])
-  const [isLoading,setIsLoading] = useState(false)
-  const [isSwitchLoading,setIsSwitchLoading] = useState(false)
-  const handleDeleteMessage =(messageId:string)=>{
-    setMessages(messages.filter((message)=>message._id!==messageId))
-  }
-  return (
-    <div>
-
-      
-    </div>
-  )
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSwitchLoading, setIsSwitchLoading] = useState(false);
+  const handleDeleteMessage = (messageId: string) => {
+    setMessages(messages.filter((message) => message._id !== messageId));
+  };
+  const { data: session } = useSession();
+  const form = useForm({
+    resolver: zodResolver(acceptMessageSchema),
+  });
+  const { register, watch, setValue } = form;
+  const acceptMessages = watch("acceptMessages");
+  const fetchAcceptMessage = useCallback(async () => {
+    setIsSwitchLoading(true);
+    try {
+      const response = await axios.get<ApiResponse>("/api/accept-messages");
+      setValue("acceptMessages", response.data.isAcceptingMessage);
+    } catch (error) {
+      const axiosError = error as AxiosError<ApiResponse>
+      toast("error",{
+        description:axiosError.response?.data.message||"failed to fetch message settings."
+      })
+    }finally{
+      setIsSwitchLoading(false);
+    }
+  }, [setValue]);
+  return <div></div>;
 }
 
-export default page
+export default page;
